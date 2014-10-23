74278c88d3558994a00c4ad89111f0ae244c57de
wputra <wasis.putra@gmail.com>
2014-10-23 15:15:41 +0700

    include zabbix node

--- main-script/115-update-bash.sh
+++ main-script/115-update-bash.sh
@@ -5,7 +5,7 @@
 ##### but still, better safe than sorry
 ##### WARNING WARNING WARNING : NEVER, EVER, do "apt-get upgrade", since it will upgrade ALL packages
 
-ALL=$( fuel node | grep -e compute -e ceph -e controller | awk '//{print $1}' );
+ALL=$( fuel node | grep -e ready | awk '//{print $1}' );
 for i in $ALL; do
 	rsync -avz script/sources.list node-$i:/etc/apt/
 	ssh node-$i "apt-get update && apt-get --only-upgrade install bash"
