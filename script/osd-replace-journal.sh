d10a096c3e3fa73d9e2e84f5f00c9ed7bdeead4a
wputra <wasis.putra@gmail.com>
2014-10-23 13:20:03 +0700

    remove 'list' command

--- script/osd-replace-journal.sh
+++ script/osd-replace-journal.sh
@@ -4,8 +4,8 @@ ID=$( cat /root/part.txt );
 x=4;
 
 for i in $ID; do
-	ls -al && stop ceph-osd id=$i && ceph-osd -i $i --flush-journal
-			rm -f /var/lib/ceph/osd/ceph-$i/journal && ln -s /dev/nvme0n1p$x /var/lib/ceph/osd/ceph-$i/journal && ls -al
+	stop ceph-osd id=$i && ceph-osd -i $i --flush-journal
+			rm -f /var/lib/ceph/osd/ceph-$i/journal && ln -s /dev/nvme0n1p$x /var/lib/ceph/osd/ceph-$i/journal
 			x=$((x + 1))
 	ceph-osd -i $i --mkjournal && start ceph-osd id=$i
 done
