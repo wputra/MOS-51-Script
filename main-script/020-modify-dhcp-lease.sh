264d9b3990bbb6d2c61c0dbe49363b738212bd32
wputra <wasis.putra@gmail.com>
2014-10-29 18:30:55 +0700

    add variable controller

--- main-script/020-modify-dhcp-lease.sh
+++ main-script/020-modify-dhcp-lease.sh
@@ -3,6 +3,7 @@
 ##### modify neutron value to openstack deafult
 ##### openstack default value will lighten neutron load compared to Mirantis value
 
+CONT=$( fuel node | grep -e controller | awk '//{print $1}' );
 for i in $CONT; do
 	ssh node-$i "sed -i 's/dhcp_lease_duration = 120/dhcp_lease_duration = 86400/g' /etc/neutron/neutron.conf"
 done
