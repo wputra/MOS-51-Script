f4da1fa1885c305a76ef363d56659f1d21d9710b
wputra <wasis.putra@gmail.com>
2014-10-29 21:18:33 +0700

    add variable controller and compute

--- main-script/030-enable-secure-vnc.sh
+++ main-script/030-enable-secure-vnc.sh
@@ -2,6 +2,7 @@
 
 ###### enable secure vnc console and restart nova in controller node
 
+CONT=$( fuel node | grep -e controller | awk '//{print $1}' );
 for i in $CONT; do
 	rsync -avz /root/cbncloudstack node-$i:/etc/ssl
 	ssh node-$i "sed -i '2issl_only=true' /etc/nova/nova.conf"
@@ -12,6 +13,7 @@ done
 	
 # update vnc setting and restart nova in compute node
 
+COMP=$( fuel node | grep -e compute | awk '//{print $1}' );
 for i in $COMP; do
 	ssh node-$i "sed -i 's|start_guests_on_host_boot|resume_guests_state_on_host_boot|g' /etc/nova/nova.conf"
 	ssh node-$i "sed -i 's|http://103.24.12.2|https://indigo.cbncloudstack.com|g' /etc/nova/nova.conf"
